0e59ecad49c962ba691fe2babb493dbd95832ade
Fimil Faneea <fimilfaneea@gmail.com>
2025-03-18 14:22:27 +0530

    retrigger

--- app/page.tsx
+++ app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
     <Container maxW="container.md" py={10}>
       <Box
         textAlign="center"
-        mt={10}
+        
         py={2}
         borderTop="1px solid"
         borderColor="gray.200"
