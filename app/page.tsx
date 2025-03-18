7706d3fc62b39e247c8f94182fd60f9f5f24d1ea
Fimil Faneea <fimilfaneea@gmail.com>
2025-03-18 14:33:58 +0530

    retrigger

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
 
   return (
     <Container maxW="container.md">
-      <Box textAlign="center" borderTop="1px solid" borderBottom="1px solid" borderColor="gray.200" >
+      <Box textAlign="right" borderTop="1px solid" borderBottom="1px solid" borderColor="gray.200" >
         <Text fontSize="sm" mb={3}>
           Created by{" "}
           <Box
